fdb25a175afa3777f637eaf971e7b2ab3cfded00
kiriliodas <159885340+kiriliodas@users.noreply.github.com>
2025-03-31 15:24:11 +0200

    Update script.js

--- script.js
+++ script.js
@@ -402,7 +402,7 @@ document.addEventListener('DOMContentLoaded', () => {
         const particleShapes = ['circle', 'triangle', 'square'];
         
         // Créer 50 particules
-        for (let i = 0; i < 100; i++) {
+        for (let i = 0; i < 10000; i++) {
             const particle = document.createElement('div');
             particle.classList.add('particle');
             
